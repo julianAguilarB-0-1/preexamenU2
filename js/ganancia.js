57496007a798691a23d95ba4dd2bb70e2b0a4163
Julián Aguilar <julian.aguilar0127@gmail.com>
2025-10-23 19:18:27 -0700

    Se agrego el Pre Examen 2

--- js/ganancia.js
+++ js/ganancia.js
@@ -1,17 +1,26 @@
-const btnCalcular = document.getElementById("calcular");
-const numProd = document.getElementById("NumeroProducto");
-const nameProd = document.getElementById("NombreProducto");
 const precioVenta = document.getElementById("PrecioV");
 const precioCompra = document.getElementById("PrecioC");
 const cantidad = document.getElementById("Cantidad");
+const btnCalcular = document.getElementById("calculos");
+const btnCalcular2 = document.getElementById("calcular");
+const mostrar = document.getElementById("mostrar");
 
 
 
-//funciones
-function calcularGanancia(){
+function calcularGanancia() {
+    let pv = parseFloat(precioVenta.value);
+    let pc = parseFloat(precioCompra.value);
+    let cant = parseInt(cantidad.value);
 
+    let totalcompra = pc * cant;
+    let totalventa = pv * cant;
+    let ganancia = totalventa - totalcompra;
 
+    document.getElementById("TotalCompra").value = totalcompra.toFixed(2);
+    document.getElementById("TotalVenta").value = totalventa.toFixed(2);
+    document.getElementById("Ganancia").value = ganancia.toFixed(2);
+    mostrar.innerHTML ="$"+ganancia.toFixed(2);
 }
 
-//eventos
 btnCalcular.addEventListener("click", calcularGanancia);
+btnCalcular2.addEventListener("click", calcularGanancia);
